7eea9d494fb08fa5891ab71855132833c4b48481
Rishabh <rishabhyadav0227@gmail.com>
2025-07-30 21:30:54 +0530

    added console

--- array/script.js
+++ array/script.js
@@ -1,6 +1,16 @@
-const list=[2,3,10];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector('.color');
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
+console.log(getRandomNumber())
 
-console.log(list)
-
-const a=5
-console.log(a)
+btn.addEventListener("click", function () {
+        let hexcolor = '#'
+        for(let i = 0; i < 6 ; i++ ){
+            hexcolor += hex[getRandomNumber()]
+        }
+        color.textContent = hexcolor;
+        document.body.style.background = hexcolor
+})
